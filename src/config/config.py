736c666289eeb435ac7408af4e268f624260cc0c
Amulya Paritosh <amulya1171@gmail.com>
2023-08-01 13:35:22 +0530

    resolved crassing problem

--- src/config/config.py
+++ src/config/config.py
@@ -1,10 +1,11 @@
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
-from pathlib import Path
+
 load_dotenv()
 
 class Config:
 	API_URL = "http://127.0.0.1:5000"
 	GITHUB_ACCESS_TOKEN = os.getenv("ACCESS_TOKEN", "")
-	LOCAL_COMPONENT_PATH = Path("local_storage")
+	LOCAL_COMPONENT_PATH = Path("test/local_storage")
