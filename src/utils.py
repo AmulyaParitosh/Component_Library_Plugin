9039dcbc59c51e5ebf74c926ec0a502898958ac2
Amulya Paritosh <amulya1171@gmail.com>
2024-08-08 18:36:53 +0530

    added a function to import FreeCAD in a platform independent way

--- src/utils.py
+++ src/utils.py
@@ -49,6 +49,7 @@ def singleton(cls: Type):
 
 
 def module_setup():
+    add_freecad_to_path()
 
     try:
         import FreeCAD
@@ -66,3 +67,42 @@ def setup_config(config):
     from .config import config as global_config
 
     global_config = config
+
+
+def add_freecad_to_path():
+    from pathlib import Path
+    import sys
+    import platform
+    import subprocess
+    import shutil
+
+    FREECAD_COMMANDS = ("FreeCADCmd", "freecadcmd")
+
+    fc_command = next(
+        (command for command in FREECAD_COMMANDS if shutil.which(command)), None
+    )
+
+    if not fc_command:
+        print(
+            f"Neither of the following commands are available: {', '.join(FREECAD_COMMANDS)}"
+        )
+        sys.exit(1)
+
+    fc_home_path = subprocess.getoutput(
+        f"{fc_command} --get-config AppHomePath"
+    ).splitlines()[-1]
+
+    os_name = platform.system()
+    fc_path = Path(fc_home_path)
+
+    if os_name == "Windows":
+        fc_path = fc_path / "bin"
+    elif os_name == "Darwin":
+        fc_path = fc_path / "lib"
+    elif os_name == "Linux":
+        fc_path = fc_path / "lib"
+    else:
+        print("Unsupported OS")
+        sys.exit(1)
+
+    sys.path.append(str(fc_path))
