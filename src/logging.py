0472bfa2e3dc681253055a47b8b5a9c14386e003
Amulya Paritosh <amulya1171@gmail.com>
2024-08-09 01:58:27 +0530

    refactor: Update logging.py to use a more concise log message format

--- src/logging.py
+++ src/logging.py
@@ -3,15 +3,13 @@ import logging
 from FreeCAD import Console
 
 
-class Logger(logging.Logger):
+class Logger(logging.getLoggerClass()):
 
     def __init__(self, name, level=logging.DEBUG):
         super().__init__(name, level)
 
         self.setLevel(level)
-        self.formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
+        self.formatter = logging.Formatter("%(asctime)s [%(levelname)s] - %(message)s")
         self.console_handler = logging.StreamHandler()
         self.console_handler.setFormatter(self.formatter)
         self.addHandler(self.console_handler)
@@ -22,27 +20,27 @@ class Logger(logging.Logger):
 
     def debug(self, msg: str) -> None:
         if self.isEnabledFor(logging.DEBUG):
-            Console.PrintLog(msg)
+            Console.PrintLog(msg + "\n")
         return super().debug(msg)
 
     def info(self, msg: str) -> None:
         if self.isEnabledFor(logging.INFO):
-            Console.PrintMessage(msg)
+            Console.PrintMessage(msg + "\n")
         return super().info(msg)
 
     def warning(self, msg: str) -> None:
         if self.isEnabledFor(logging.WARNING):
-            Console.PrintWarning(msg)
+            Console.PrintWarning(msg + "\n")
         return super().warning(msg)
 
     def exception(self, msg: str) -> None:
         if self.isEnabledFor(logging.ERROR):
-            Console.PrintError(msg)
+            Console.PrintError(msg + "\n")
         return super().exception(msg)
 
     def critical(self, msg: str) -> None:
         if self.isEnabledFor(logging.CRITICAL):
-            Console.PrintCritical(msg)
+            Console.PrintCritical(msg + "\n")
         return super().critical(msg)
 
 
